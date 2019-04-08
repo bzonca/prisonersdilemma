aed5321cb6217803f76504bbe2b3e29f8982d5c9
Blake <blakezonca@gmail.com>
2019-04-08 11:36:27 -0400

    Implement javafx

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,12 +1,42 @@
 import java.util.Scanner;
 
-public class GameManager {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GameManager extends Application{
 
     private boolean isProgramActive;
 
-    public static void main(String[] args){
-        GameManager gm = new GameManager();
-        gm.runProgramLoop();
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+
+        //TODO : Port all interaction to javafx
+        //GameManager gm = new GameManager();
+        //gm.runProgramLoop();
     }
 
     private void runProgramLoop(){
